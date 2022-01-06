f315c7c3c234ee3d49c916dbebab92d82b3eb4f0
Jared Hanson <jaredhanson@gmail.com>
2022-01-06 07:13:38 -0800

    WIP: chapter test.

--- test/handlers/chapter.test.js
+++ test/handlers/chapter.test.js
@@ -3,13 +3,20 @@
 var expect = require('chai').expect;
 var chai = require('chai');
 var factory = require('../../lib/handlers/chapter');
+var path = require('path');
+var GitBook = require('../../lib/gitbook');
 
 
 describe('handlers/chapter', function() {
   
   it.skip('should do something', function(done) {
+    var book = new GitBook(path.resolve(__dirname, '../data/chapters'));
     
-    chai.kerouac.page(factory('Example Book', 'book', 'README.md', 'book/chapter'))
+    chai.kerouac.page(factory(book, 'book/chapter'))
+      .request(function(page) {
+        //page.params = { 0: 'index' };
+        page.params = { 0: 'writing' };
+      })
       .finish(function() {
         expect(1).to.equal(2);
         done();
