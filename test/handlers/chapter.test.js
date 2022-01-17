e85759fd87531a4a6adaeb5aa8a6bdaa6d457741
Jared Hanson <jaredhanson@gmail.com>
2022-01-17 14:16:20 -0800

    Add tests.

--- test/handlers/chapter.test.js
+++ test/handlers/chapter.test.js
@@ -78,44 +78,48 @@ describe('handlers/chapter', function() {
       .generate();
   }); // should render chapter
   
-  it.skip('should render with table of contents containing parts', function(done) {
-    var book = new GitBook(path.resolve(__dirname, '../data/parts'));
+  it.only('should render with table of contents containing parts', function(done) {
+    var book = new GitBook(path.resolve(__dirname, '../data/books/parts'));
     
     chai.kerouac.page(factory(book, 'book/chapter'))
       .request(function(page) {
-        //page.params = { 0: 'index' };
-        page.params = { 0: 'writing' };
+        page.params = { 0: 'part1/chapter1' };
       })
       .finish(function() {
         expect(this).to.render('book/chapter')
-          .with.locals({
-            title: 'Parts: Writing is nice'
-          })
-          .and.beginWith.content('# Writing').of.format('md');
-          
-        expect(this.locals.toc).to.deep.equal([
-          { text: 'Part I' },
-          [
-            { title: 'Writing is nice', href: 'part1/writing.md' },
-            { title: 'GitBook is nice', href: 'part1/gitbook.md', children: null }
-          ],
-          { text: 'Part II' },
-          [
-            { title: 'We love feedback', href: 'part2/feedback_please.md' },
-            { title: 'Better tools for authors', href: 'part2/better_tools.md', children: null }
-          ],
-          { divider: true },
-          [
-            { title: 'Last part without title', href: 'part3/title.md' }
-          ]
+          .and.beginWith.content('# Chapter 1').of.format('md');
+        
+        expect(this.locals.book).to.deep.equal({
+          title: 'Parts Example'
+        });
+        expect(this.locals.page).to.deep.equal({
+          title: 'Chapter 1'
+        });
+        expect(this.locals.summary).to.deep.equal([
+          {
+            title: 'Part I',
+            chapters: [
+              { title: 'Chapter 1', href: 'part1/chapter1.md' },
+              { title: 'Chapter 2', href: 'part1/chapter2.md' }
+            ]
+          },
+          {
+            title: 'Part II',
+            chapters: [
+              { title: 'Chapter 3', href: 'part2/chapter3.md' },
+              { title: 'Chapter 4', href: 'part2/chapter4.md' }
+            ]
+          },
+          {
+            divider: true,
+            chapters: [
+              { title: 'Chapter 5', href: 'part3/chapter5.md' }
+            ]
+          }
         ]);
-          
         expect(this.createdAt).to.be.an.instanceof(Date);
         expect(this.modifiedAt).to.be.an.instanceof(Date);
         done();
-      })
-      .next(function(err) {
-        console.log(err);
       })
       .generate();
   }); // should render with table of contents containing parts
