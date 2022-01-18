26f9a9e34865d6ea8293b7983156780ca81c614e
Jared Hanson <jaredhanson@gmail.com>
2022-01-17 16:23:58 -0800

    More accurate summary mapping.

--- test/handlers/chapter.test.js
+++ test/handlers/chapter.test.js
@@ -27,14 +27,16 @@ describe('handlers/chapter', function() {
         expect(this.locals.page).to.deep.equal({
           title: undefined
         });
-        expect(this.locals.summary).to.deep.equal([
-          {
-            articles: [
-              { title: 'Chapter 1', path: 'chapter-1.md' },
-              { title: 'Chapter 2', path: 'chapter-2.md' }
-            ]
-          }
-        ]);
+        expect(this.locals.summary).to.deep.equal({
+          parts: [
+            {
+              articles: [
+                { title: 'Chapter 1', path: 'chapter-1.md' },
+                { title: 'Chapter 2', path: 'chapter-2.md' }
+              ]
+            }
+          ]
+        });
         expect(this.createdAt).to.be.an.instanceof(Date);
         expect(this.modifiedAt).to.be.an.instanceof(Date);
         done();
@@ -63,14 +65,16 @@ describe('handlers/chapter', function() {
         expect(this.locals.page).to.deep.equal({
           title: 'Chapter 1'
         });
-        expect(this.locals.summary).to.deep.equal([
-          {
-            articles: [
-              { title: 'Chapter 1', path: 'chapter-1.md' },
-              { title: 'Chapter 2', path: 'chapter-2.md' }
-            ]
-          }
-        ]);
+        expect(this.locals.summary).to.deep.equal({
+          parts: [
+            {
+              articles: [
+                { title: 'Chapter 1', path: 'chapter-1.md' },
+                { title: 'Chapter 2', path: 'chapter-2.md' }
+              ]
+            }
+          ]
+        });
         expect(this.createdAt).to.be.an.instanceof(Date);
         expect(this.modifiedAt).to.be.an.instanceof(Date);
         done();
@@ -95,28 +99,30 @@ describe('handlers/chapter', function() {
         expect(this.locals.page).to.deep.equal({
           title: 'Chapter 1'
         });
-        expect(this.locals.summary).to.deep.equal([
-          {
-            title: 'Part I',
-            articles: [
-              { title: 'Chapter 1', path: 'part1/chapter1.md' },
-              { title: 'Chapter 2', path: 'part1/chapter2.md' }
-            ]
-          },
-          {
-            title: 'Part II',
-            articles: [
-              { title: 'Chapter 3', path: 'part2/chapter3.md' },
-              { title: 'Chapter 4', path: 'part2/chapter4.md' }
-            ]
-          },
-          {
-            divider: true,
-            articles: [
-              { title: 'Chapter 5', path: 'part3/chapter5.md' }
-            ]
-          }
-        ]);
+        expect(this.locals.summary).to.deep.equal({
+          parts: [
+            {
+              title: 'Part I',
+              articles: [
+                { title: 'Chapter 1', path: 'part1/chapter1.md' },
+                { title: 'Chapter 2', path: 'part1/chapter2.md' }
+              ]
+            },
+            {
+              title: 'Part II',
+              articles: [
+                { title: 'Chapter 3', path: 'part2/chapter3.md' },
+                { title: 'Chapter 4', path: 'part2/chapter4.md' }
+              ]
+            },
+            {
+              divider: true,
+              articles: [
+                { title: 'Chapter 5', path: 'part3/chapter5.md' }
+              ]
+            }
+          ]
+        });
         expect(this.createdAt).to.be.an.instanceof(Date);
         expect(this.modifiedAt).to.be.an.instanceof(Date);
         done();
