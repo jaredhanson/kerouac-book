7fce752a4540e147f9ac5024762d67db9c136cae
Jared Hanson <jaredhanson@gmail.com>
2022-05-08 07:17:13 -0700

    Fix tests.

--- test/handlers/chapter.test.js
+++ test/handlers/chapter.test.js
@@ -9,52 +9,6 @@ var GitBook = require('../../lib/gitbook');
 
 describe('handlers/chapter', function() {
   
-  it('should render preface', function(done) {
-    var book = new GitBook(path.resolve(__dirname, '../data/books/simple'));
-    
-    chai.kerouac.page(factory(book, 'book/chapter'))
-      .request(function(page) {
-        page.params = { 0: 'index' };
-      })
-      .finish(function() {
-        expect(this).to.render('book/chapter')
-          .with.locals({ title: 'Example Book' })
-          .and.beginWith.content('# Example Book').of.format('md');
-        
-        expect(this.locals.book).to.deep.equal({
-          title: 'Example Book'
-        });
-        expect(this.locals.gitbook.time).to.be.an.instanceof(Date);
-        expect(this.locals.gitbook).to.deep.equal({
-          time: this.locals.gitbook.time
-        });
-        expect(this.locals.page).to.deep.equal({
-          title: 'Example Book'
-        });
-        expect(this.locals.file.mtime).to.be.an.instanceof(Date);
-        expect(this.locals.file).to.deep.equal({
-          path: 'README.md',
-          mtime: this.locals.file.mtime,
-          type: 'markdown'
-        });
-        expect(this.locals.summary).to.deep.equal({
-          parts: [
-            {
-              articles: [
-                { title: 'Chapter 1', path: 'chapter-1.md' },
-                { title: 'Chapter 2', path: 'chapter-2.md' }
-              ]
-            }
-          ]
-        });
-        expect(this.createdAt).to.be.an.instanceof(Date);
-        expect(this.modifiedAt).to.be.an.instanceof(Date);
-        done();
-      })
-      .next(done)
-      .generate();
-  }); // should render preface
-  
   it('should render chapter', function(done) {
     var book = new GitBook(path.resolve(__dirname, '../data/books/simple'));
     
---
+++ test/handlers/preface.test.js
@@ -0,0 +1,57 @@
+/* global describe, it, expect */
+
+var expect = require('chai').expect;
+var chai = require('chai');
+var factory = require('../../lib/handlers/preface');
+var path = require('path');
+var GitBook = require('../../lib/gitbook');
+
+
+describe('handlers/preface', function() {
+  
+  it('should render preface', function(done) {
+    var book = new GitBook(path.resolve(__dirname, '../data/books/simple'));
+    
+    chai.kerouac.page(factory(book, 'book/chapter'))
+      .request(function(page) {
+      })
+      .finish(function() {
+        expect(this).to.render('book/chapter')
+          .with.locals({ title: 'Example Book' })
+          .and.beginWith.content('# Example Book').of.format('md');
+        
+        expect(this.locals.book).to.deep.equal({
+          title: 'Example Book'
+        });
+        expect(this.locals.gitbook.time).to.be.an.instanceof(Date);
+        expect(this.locals.gitbook).to.deep.equal({
+          time: this.locals.gitbook.time
+        });
+        expect(this.locals.page).to.deep.equal({
+          title: 'Example Book'
+        });
+        expect(this.locals.file.mtime).to.be.an.instanceof(Date);
+        expect(this.locals.file).to.deep.equal({
+          path: 'README.md',
+          mtime: this.locals.file.mtime,
+          type: 'markdown'
+        });
+        expect(this.locals.summary).to.deep.equal({
+          parts: [
+            {
+              articles: [
+                { title: 'Chapter 1', path: 'chapter-1.md' },
+                { title: 'Chapter 2', path: 'chapter-2.md' }
+              ]
+            }
+          ]
+        });
+        expect(this.createdAt).to.be.an.instanceof(Date);
+        expect(this.modifiedAt).to.be.an.instanceof(Date);
+        done();
+      })
+      .next(done)
+      .generate();
+  }); // should render preface
+  
+});
